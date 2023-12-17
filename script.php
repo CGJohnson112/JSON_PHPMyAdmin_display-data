ed7e588501caa3aad6605ae9e0a7c5258e740915
Chris G Johnson <chrisjohnson112@yahoo.com>
2023-12-17 08:29:34 -0600

    reuploading after successful MAMP testing

--- script.php
+++ script.php
@@ -1,12 +1,11 @@
-
-
 <?php
+//first three lines are troubleshooting code 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
-    $mysqli = new mysqli('localhost', 'root', '', 'tutorials');
+//this gets code from my database on localhost
+    $mysqli = new mysqli('localhost', 'root', 'root', 'tutorials');
     if($mysqli->connect_errno != 0){
         echo $mysqli->connect_error;
         exit();
@@ -15,7 +14,8 @@ error_reporting(E_ALL);
     $sql = "SELECT * FROM products";
     $results = $mysqli->query($sql);
     while($product = $results->fetch_assoc()){
-        //can enter any category in your phpmyadmin SQL table you like
+        //can enter any category in your phpmyadmin SQL table you like,
+        //only set up to enter entire array into data.json
         $products[] = $product;
         
     }
@@ -25,10 +25,7 @@ error_reporting(E_ALL);
     //prints JSON data on screen --can use Google JSON to pretty print this
     print($encoded_data);
 
-
-    //currently will NOT build the data.json file from phpmyadmin after being called. 
-    //WIP: prob has more to do with XAMMP server settings 
-    //file_put_contents($file_name, $encoded_data);
+    file_put_contents($file_name, $encoded_data);
     
     
 ?>
